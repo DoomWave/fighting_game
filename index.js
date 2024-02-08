42d37073aed435c5cd9e5b1d61af657a2b81680c
DoomWave <123853739+DoomWave@users.noreply.github.com>
2024-02-07 18:33:30 -0800

    putting the files and making the frames animation.

---
+++ index.js
@@ -0,0 +1,31 @@
+const GameViewport = {
+  WIDTH: 384,
+  HEIGHT: 224,
+};
+
+window.onload = function() {
+    const canvasEl = document.querySelector('canvas');
+    const context = canvasEl.getContext('2d');
+
+    canvasEl.width = GameViewport.WIDTH;
+    canvasEl.height = GameViewport.HEIGHT;
+
+    const venom = document.querySelector('img');
+
+    function frame() {
+      context.strokeStyle = 'yellow';
+      context.moveTo(0, 0);
+      context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+      context.moveTo(GameViewport.WIDTH, 0)
+      context.lineTo(0, GameViewport.HEIGHT);
+      context.stroke(); 
+  
+      context.drawImage(venom, 0, 0);
+    }
+
+    window.requestAnimationFrame(frame);
+
+    console.log(context);
+
+    console.log(context)
+}
